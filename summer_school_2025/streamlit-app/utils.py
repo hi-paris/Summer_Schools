ca26261217868bdc21f5800a28ab7f8e28fe6ae2
Marguerite Leang <marguerite.leang@gmail.com>
2025-07-02 18:11:23 +0200

    Handle path to models for streamlit cloud deployment.

--- summer_school_2025/streamlit-app/utils.py
+++ summer_school_2025/streamlit-app/utils.py
@@ -7,7 +7,12 @@ from streamlit_gsheets import GSheetsConnection
 
 @st.cache_data()
 def load_artifact(file):
-    path_artifact = r"saved_models/"
+    # Uncomment for local deployment
+    # path_artifact = r"saved_models/"
+    
+    # For streamlit cloud deployment, specify the relative path to the saved models from the root of the github repository
+    path_artifact = r"summer_school_2025/streamlit-app/saved_models/saved_models/"
+    
     artifact = joblib.load(os.path.join(path_artifact, file))
     return artifact
 
